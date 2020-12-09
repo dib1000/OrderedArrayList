9cde86e8a1afa05f37d8eb3914ca6c73a5a4910e
Emma Buller <dib5017@gmail.com>
2020-12-09 13:11:11 -0500

    Made constructor w/o initial capacity

---
+++ Tester.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+public class Tester {
+  public static void main(String[] args) {
+      NoNullArrayList<String> t = new NoNullArrayList<String>();
+      System.out.println(t.size());
+  }
+}

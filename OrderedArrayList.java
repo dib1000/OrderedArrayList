75ed4b030b4f15e0fa47c90bd0df64b6cb0f00d3
Emma Buller <dib5017@gmail.com>
2020-12-12 19:57:22 -0500

    Tested first constructor

---
+++ OrderedArrayList.java
@@ -0,0 +1,5 @@
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+  public OrderedArrayList() {
+    super();
+  }
+}
---
+++ OrderedTester.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+public class OrderedTester {
+  public static void main(String[] args) {
+    OrderedArrayList a = new OrderedArrayList<Integer>();
+    System.out.println(a.size());
+  }
+}

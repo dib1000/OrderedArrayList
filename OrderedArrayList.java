0473bc4d3bf791b39eeb34c6319deec03d766d07
Emma Buller <dib5017@gmail.com>
2020-12-13 17:00:50 -0500

    Tested add method for OrderedArrayList

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -5,4 +5,22 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int InitialCapacity) {
     super(InitialCapacity);
   }
+
+  private int findIndex(T element) {
+    int index = 0;
+    for(int i = 0; i<size(); i++) {
+      if(element.compareTo(get(i))>=0) {
+        index++;
+      }
+    }
+    return index;
+  }
+
+  public boolean add(T element) {
+    if (element==null) {
+      throw new IllegalArgumentException("Null isn't a valid value");
+    }
+    super.add(findIndex(element), element);
+    return true;
+  }
 }
--- OrderedTester.java
+++ OrderedTester.java
@@ -1,9 +1,28 @@
 import java.util.ArrayList;
 public class OrderedTester {
   public static void main(String[] args) {
+    System.out.println("Testing constructors");
     OrderedArrayList a = new OrderedArrayList<Integer>();
     System.out.println(a.size());
     OrderedArrayList b = new OrderedArrayList<Integer>(50);
     System.out.println(b.size());
+    System.out.println();
+
+    System.out.println("Testing add");
+    a.add(1);
+    a.add(5);
+    a.add(4);
+    a.add(2);
+    a.add(3);
+    a.add(6);
+    for(int i = 0; i<a.size(); i++) {
+      System.out.println(a.get(i));
+    }
+    try{
+      a.add(null);
+    }
+    catch(IllegalArgumentException e) {
+      e.printStackTrace();
+    }
   }
 }
